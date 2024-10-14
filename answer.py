c82f7dc53ab3d3aa0ac0f933d8d2c803e4e19947
batoula1 <batoulaa52@gmail.com>
2024-10-14 01:16:40 -0400

    Advance Example - List

---
+++ answer.py
@@ -0,0 +1,12 @@
+
+# Problem 1
+# Using the list below, use a Class method to add the string "skirt" to the end of the list.
+clothing= ['socks', 'pants', 'shirt', 'polo']
+clothing.append("skirt")
+print(clothing)
+
+# Problem 2
+# Using the same list as above, add a list to the 2nd index that includes the following strings:
+# 'suit jacket', 'suit pants', 'tie', 'button down'
+clothing.insert(2,['suit jacket', 'suit pants', 'tie', 'button down'])
+print(clothing)
